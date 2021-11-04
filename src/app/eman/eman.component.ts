5d2486d435323ea1b2c1f00463547bd27ca109c1
Eman <2018801018@ses.yu.edu.jo>
2021-11-04 22:44:01 +0200

    addEman

---
+++ src/app/eman/eman.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eman',
+  templateUrl: './eman.component.html',
+  styleUrls: ['./eman.component.css']
+})
+export class EmanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
